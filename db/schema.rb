448d4d432b06698d9aad2f3efac1569ab5500595
Heitor Cardoso <heitorcardosoo@gmail.com>
2023-06-06 18:24:54 -0300

    updates-segunda1

--- db/schema.rb
+++ db/schema.rb
@@ -42,6 +42,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_06_181407) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "favourite_movies", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "recommendations", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
