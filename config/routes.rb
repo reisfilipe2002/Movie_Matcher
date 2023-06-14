e6911d52bb25004be160673d12795c5a59ed8b24
Fernando Almeida <nando.a.g.martines@gmail.com>
2023-06-14 17:15:55 -0300

    profile page ok

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :favourite_movies
   get 'users/:id/chooseafavorite', to: 'profiles#choose_favorite_movies', as: :selection
 
-  get 'profile/users/:id' => 'profiles#show', as: :profile
+  get '/profile', to: 'profiles#show', as: :profile
   delete 'profiles/:id', to: 'profiles#destroy', as: :delete_profile
 
   devise_for :users
